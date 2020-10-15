b60c0354bc7b9b45404bf5ef0b40bc4bfa64fbdc
Daniel <danisan9490@gmail.com>
2020-10-15 10:52:27 +0100

    #7 Add UseReducer to get Proyects

--- client/src/context/proyectos/proyectoState.js
+++ client/src/context/proyectos/proyectoState.js
@@ -2,10 +2,21 @@ import React, { useReducer } from 'react';
 
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
-import { FORMULARIO_PROYECTO } from '../../types';
+import {
+  FORMULARIO_PROYECTO,
+  OBTENER_PROYECTOS
+} from '../../types';
 
 const ProyectoState = props => {
+  const proyectos = [
+    { id: 1, nombre: 'Tienda Virtual' },
+    { id: 2, nombre: 'Intranet' },
+    { id: 3, nombre: 'Diseño de Sitio web' },
+    { id: 4, nombre: 'MERN ' }
+  ]
+
   const initialState = {
+    proyectos: [],
     formulario: false
   }
 
@@ -19,11 +30,21 @@ const ProyectoState = props => {
     })
   }
 
+  // Obtener los proyectos
+  const obtenerProyectos = () => {
+    dispatch({
+      type: OBTENER_PROYECTOS,
+      payload: proyectos
+    })
+  }
+
   return (
     <proyectoContext.Provider
       value={{
+        proyectos: state.proyectos,
         formulario: state.formulario,
-        mostrarFormulario
+        mostrarFormulario,
+        obtenerProyectos
       }}
     >
       {props.children}
