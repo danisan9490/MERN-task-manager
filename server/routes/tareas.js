9dabd5039640f622a0d9acb9f8df60ed964c5789
Daniel <danisan9490@gmail.com>
2020-10-15 17:47:56 +0100

    #9 Get Put Delete Task

--- server/routes/tareas.js
+++ server/routes/tareas.js
@@ -15,4 +15,21 @@ router.post('/',
   tareaController.crearTarea
 );
 
+router.get('/',
+  auth,
+  tareaController.obtenerTareas
+);
+
+// Actualizar tarea
+router.put('/:id',
+  auth,
+  tareaController.actualizarTarea
+);
+
+// Eliminar tarea
+router.delete('/:id',
+  auth,
+  tareaController.eliminarTarea
+);
+
 module.exports = router; 
