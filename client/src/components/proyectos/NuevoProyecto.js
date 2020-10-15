c0e59ef4f34c2fe9b553a8afc1750bc692d74ce9
Daniel <danisan9490@gmail.com>
2020-10-15 08:46:12 +0100

    #2 Add LogIn and New Project Form

---
+++ client/src/components/proyectos/NuevoProyecto.js
@@ -0,0 +1,62 @@
+import React, { Fragment, useState } from 'react';
+
+const NuevoProyecto = () => {
+  // State para Proyecto
+  const [proyecto, guardarProyecto] = useState({
+    nombre: ''
+  });
+
+  // Extraer nombre de proyecto
+  const { nombre } = proyecto;
+
+  // Lee los contenidos del input
+  const onChangeProyecto = e => {
+    guardarProyecto({
+      ...proyecto,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  // Cuando el usuario envia un proyecto
+  const onSubmitProyecto = e => {
+    e.preventDefault();
+
+    // Validar el proyecto
+
+
+    // agregar al state
+
+    // Reiniciar el form
+  }
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-block btn-primario"
+        onSubmit={onSubmitProyecto}
+      >Nuevo Proyecto</button>
+
+      <form
+        className="formulario-nuevo-proyecto"
+      >
+        <input
+          type="text"
+          className="input-text"
+          placeholder="Nombre Proyecto"
+          name="nombre"
+          value={nombre}
+          onChange={onChangeProyecto}
+        />
+
+        <input
+          type="submit"
+          className="btn btn-primario btn-block"
+          value="Agregar Proyecto"
+        />
+
+      </form>
+    </Fragment>
+  );
+}
+
+export default NuevoProyecto; 
