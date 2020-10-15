2912977166cbacd470f139b34c0fefc9d969b548
Daniel <danisan9490@gmail.com>
2020-10-15 17:58:53 +0100

    #11 Fix Put Tasks

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,10 +8,7 @@ const auth = require('../middleware/auth');
 
 // api/auth
 router.post('/',
-  [
-    check('email', 'Agrega un email válido').isEmail(),
-    check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6 })
-  ],
+
   authController.autenticarUsuario
 );
 
